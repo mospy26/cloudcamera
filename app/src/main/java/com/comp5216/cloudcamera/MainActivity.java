b65432509abb68d1efef83aebd0c6ce0c67104c9
mospy26 <m.mustafaahmed@hotmail.com>
2020-10-07 16:56:21 +1100

    Grid view working with sample image url

--- app/src/main/java/com/comp5216/cloudcamera/MainActivity.java
+++ app/src/main/java/com/comp5216/cloudcamera/MainActivity.java
@@ -3,6 +3,12 @@ package com.comp5216.cloudcamera;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridView;
+
+import com.comp5216.cloudcamera.adapter.GridViewPhotosAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initFabButton();
+        initGridView();
+    }
+
+    public void initFabButton() {
+        FloatingActionButton fab = findViewById(R.id.fab_main_page);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // handle clicking fab button
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void initGridView() {
+        GridView gridView = findViewById(R.id.gridview_main_page);
+        gridView.setAdapter(new GridViewPhotosAdapter(this));
     }
 }
---
+++ app/src/main/java/com/comp5216/cloudcamera/adapter/GridViewPhotosAdapter.java
@@ -0,0 +1,61 @@
+package com.comp5216.cloudcamera.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class GridViewPhotosAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<String> imageUrls;
+    LayoutInflater layoutInflater;
+
+    public GridViewPhotosAdapter(Context context) {
+        this.context = context;
+        imageUrls = new ArrayList<>();
+        imageUrls.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png");
+    }
+
+    @Override
+    public int getCount() {
+        return imageUrls.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return imageUrls.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ImageView imageView;
+        if (view == null) {
+            imageView = new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(480, 480));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) view;
+        }
+        String url = (String) getItem(i);
+        Glide.with(context)
+                .load(url)
+                .centerCrop()
+                .placeholder(android.R.drawable.spinner_background)
+                .into(imageView);
+        return imageView;
+    }
+}
